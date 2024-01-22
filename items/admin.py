dd587775d6fbefa34d09d22fdf810732049f3af9
Hayespace <ben.hayes@hotmail.com>
2024-01-22 22:15:07 +0000

    customise admin

--- items/admin.py
+++ items/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Item
 
 # Register your models here.
-admin.site.register(Item)
+
+class ItemsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'base_price',
+        'quantity_available',
+        'image',
+    )
+
+    ordering = ('name',)
+
+admin.site.register(Item, ItemsAdmin)
